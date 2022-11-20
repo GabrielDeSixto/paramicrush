d0f28e73219af6300c1ed300f11a04c52e812b69
Gabriel <gabrielsolanodesixto@gmail.com>
2022-11-19 19:08:25 -0600

    innitial commint

---
+++ app.js
@@ -0,0 +1,23 @@
+function moverPosicionRandom(elm){
+    elm.style.position = 'absolute';
+    elm.style.top = Math.random()*(window.innerHeight - elm.offsetHeight) + 'px' ;
+    elm.style.left = Math.random()*(window.innerWidth - elm.offsetWidth) + 'px' ;
+
+}
+
+let btnSi = document.getElementById('btn_si'); 
+let bntNo = document.getElementById('btn_no');
+var divModoSexo = document.getElementsByClassName('modo_sexo')[0];
+
+bntNo.addEventListener('mouseenter',function(e){
+    moverPosicionRandom(e.target)
+});
+
+btnSi.addEventListener('click',function(e){
+    alert('Sabía que dirías que SI... Casemos ya y tengamos hijos. TE AMO!!!!❤️');
+
+    divModoSexo.style.display= 'block';
+    const cancion = new Audio('img\\img_modo_hot.mp3');
+    cancion.play();
+})
+
